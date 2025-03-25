a7355efb1d6d8831a2529dd91eb7c06f5e695881
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-03-25 14:36:59 +0200

    feat: initialised base components and add body bg color

--- Weather-App/src/App.jsx
+++ Weather-App/src/App.jsx
@@ -1,10 +1,21 @@
+import {Navbar} from './components/Navbar/Navbar';
 import './App.css'
 
 function App() {
-
+  // console.log(navigator.geolocation.getCurrentPosition((position)=> {
+  //   console.log(position)
+  // }))
   return (
     <>
-      <h1>Vite</h1>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
     </>
   )
 }
