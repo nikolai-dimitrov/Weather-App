50ae03ea2db85b3d6c0bad3b9771b0a3f8c404b6
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-03-25 17:04:07 +0200

    feat: implement header and nav ui

--- Weather-App/src/components/Navbar/Navbar.jsx
+++ Weather-App/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,32 @@
+import { IoMdPartlySunny } from "react-icons/io";
+import { BiCurrentLocation } from "react-icons/bi";
+import { FaSearch } from "react-icons/fa";
+import styles from "./navbar.module.css";
+
 export const Navbar = () => {
     return (
         <>
             <nav>
-
+                <div className={styles.logoContainer}>
+                    <IoMdPartlySunny size={50} />
+                    <span>WeatherNow</span>
+                </div>
+                <form className={styles.searchForm}>
+                    <div className={styles.inputWrapper}>
+                        <input type="text" placeholder="Search Locations" />
+                        <button className={styles.searchBtn}>
+                            <FaSearch size={20} />
+                        </button>
+                    </div>
+                    <button className={styles.locationBtn}>
+                        <BiCurrentLocation size={30} />
+                    </button>
+                </form>
+                <div className={styles.metricsContainer}>
+                    <button>°C</button>
+                    <span>/</span>
+                    <button>°F</button>
+                </div>
             </nav>
         </>
     )
