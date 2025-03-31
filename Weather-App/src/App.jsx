d4daf5249f027c3a10b959f7fe07f4920eca5704
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-03-31 17:19:10 +0300

    feat: get geolocation then fetch weather data from api with current location
    When open the app get current location and fetch data from weather api for your current location

--- Weather-App/src/App.jsx
+++ Weather-App/src/App.jsx
@@ -1,33 +1,49 @@
 import { useState, useEffect } from 'react';
 import { Navbar } from './components/Navbar/Navbar';
 import { OpeningAnimation } from './components/OpeningAnimation/OpeningAnimation';
+
+import { fetchWeatherData, extractWeatherData } from './services/weatherApiService';
 import './App.css'
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [currentLocation, setCurrentLocation] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
 
   useEffect(() => {
     const timeOut = setTimeout(() => setIsLoading(false), 4000);
     return () => clearTimeout(timeOut);
-  }, [])
+  }, []);
 
   useEffect(() => {
     getGeoLocation();
-  }, [])
+    const getWeatherData = async () => {
+      try {
+        // const weatherData = await extractWeatherData(currentLocation);
+        // setWeatherData(weatherData);
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+    // getWeatherData()
+  }, []);
 
   // TODO: Display errors into popup
   const getGeoLocation = () => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const { latitude, longitude } = position["coords"];
-        setCurrentLocation({ latitude, longitude });
-      },
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          const { latitude, longitude } = position["coords"];
+          setCurrentLocation({ latitude, longitude });
+
+          const weatherData = await extractWeatherData({ latitude, longitude });
+          setWeatherData(weatherData);
+        },
         (error) => {
-          console.log("There is problem getting user location.")
+          console.log("There is problem getting user location.");
         })
     } else {
-      console.log("Geolocation is not available.")
+      console.log("Geolocation is not available.");
     }
   }
 
---
+++ Weather-App/src/services/weatherApiService.js
@@ -0,0 +1,66 @@
+const API_KEY = "bdf42325dd664721bf0112934252503";
+const BASE_URL = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}`;
+
+export const fetchWeatherData = async (queryString) => {
+	const url = `${BASE_URL}&q=${queryString}&days=3`;
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(`Response status: ${response.status}`);
+		}
+
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.log(error.message);
+	}
+};
+
+export const extractWeatherData = async (locationParams) => {
+	let queryString = "";
+	if (typeof locationParams === "object") {
+		queryString = `${locationParams.latitude},${locationParams.longitude}`;
+	}
+
+	const weatherData = await fetchWeatherData(queryString);
+
+	const {
+		current: { humidity },
+		current: { wind_kph },
+		current: { feelslike_c },
+		current: { temp_c },
+		current: {
+			condition: { text },
+		},
+
+		location: { country },
+		location: { localtime },
+		location: { region },
+		location: { name },
+		forecast: { forecastday },
+	} = weatherData;
+
+	const {
+		astro: { sunrise },
+		astro: { sunset },
+		day: { maxtemp_c },
+		day: { mintemp_c },
+	} = forecastday[0];
+
+	return {
+		humidity,
+		wind_kph,
+		feelslike_c,
+		temp_c,
+		country,
+		region,
+		name,
+		localtime,
+		text,
+		forecastday,
+		sunrise,
+		sunset,
+		maxtemp_c,
+		mintemp_c,
+	};
+};
