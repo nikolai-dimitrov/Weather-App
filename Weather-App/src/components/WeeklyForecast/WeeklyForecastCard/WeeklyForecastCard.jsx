53c017d11c9f30bca4c6aa43607c45afd7d13914
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-04-17 20:33:29 +0300

    feat: implement weekly forecast loading-skeleton and data fade in/out functionality

--- Weather-App/src/components/WeeklyForecast/WeeklyForecastCard/WeeklyForecastCard.jsx
+++ Weather-App/src/components/WeeklyForecast/WeeklyForecastCard/WeeklyForecastCard.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-
+import { AnimatePresence, motion, easeInOut } from "motion/react";
 import Skeleton from "react-loading-skeleton";
 
 import { parseLocalTimePart } from "../../../utils/formatLocalTime";
@@ -24,8 +24,56 @@ export const WeeklyForecastCard = ({ unit, dailyWeatherData, changeHourlyForecas
     return (
         <div className={styles.cursorPointer} onClick={() => changeHourlyForecastHandler(index)}>
             <p>{shortDayName}</p>
-            {isImageLoading && <Skeleton className={globalStyles.imgSkeletonMedium} />}
-            <img className={isImageLoading ? globalStyles.displayNone : ''} src={dailyWeatherData.day.condition.icon} alt="Weather img" onLoad={onLoadImageHandler} />
+            <div className={styles.animationContainer}>
+                <AnimatePresence>
+                    {
+                        isImageLoading && (
+                            <motion.div
+                                key="skeleton"
+                                className={styles.skeletonContainer}
+                                transition={{
+                                    duration: 0.3,
+                                    ease: easeInOut,
+                                }}
+
+                                initial={{
+                                    opacity: 1,
+
+                                }}
+
+                                exit={{
+                                    opacity: 0,
+                                }}
+                            >
+                                <Skeleton className={globalStyles.imgSkeletonMedium} />
+                            </motion.div>
+
+                        )
+                    }
+                </AnimatePresence>
+                <motion.div
+                    key="image"
+                    transition={{
+                        duration: 0.3,
+                        delay: 0.2,
+                        ease: easeInOut,
+                    }}
+
+                    initial={{
+                        opacity: 0,
+                    }}
+
+                    animate={{
+                        // Ternary operator because image will mount in the same time with skeleton and animation will run under the skeleton.Target is to start animation when skeleton disappear.
+                        // When isImageLoading is false skeleton disappears and animation starts.
+                        opacity: isImageLoading ? 0 : 1,
+
+                    }}
+                >
+                    <img src={dailyWeatherData.day.condition.icon} alt="Weather img" onLoad={onLoadImageHandler} />
+
+                </motion.div>
+            </div>
             <p>{unit === "C" ? `${dailyWeatherData.day.avgtemp_c}° C` : `${dailyWeatherData.day.avgtemp_f} °F`}</p>
         </div>
     )
