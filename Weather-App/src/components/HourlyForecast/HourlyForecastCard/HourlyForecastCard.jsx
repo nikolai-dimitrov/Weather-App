5ba967f091d17417a92526ad1575f0acf99882e4
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-04-18 23:50:57 +0300

    feat: implement hourly forecast loading-skeleton and data fade in/out functionality

--- Weather-App/src/components/HourlyForecast/HourlyForecastCard/HourlyForecastCard.jsx
+++ Weather-App/src/components/HourlyForecast/HourlyForecastCard/HourlyForecastCard.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
+import { AnimatePresence, easeInOut, motion } from "motion/react";
+
 import Skeleton from "react-loading-skeleton";
 
 import 'react-loading-skeleton/dist/skeleton.css';
-
-import globalStyles from '../../../styles/global.module.css'
+import styles from './hourly-forecast-card.module.css';
+import globalStyles from '../../../styles/global.module.css';
 export const HourlyForecastCard = ({ unit, currentHourObject }) => {
     const [isImageLoading, setIsImageLoading] = useState(true);
 
@@ -12,15 +14,71 @@ export const HourlyForecastCard = ({ unit, currentHourObject }) => {
     }, [currentHourObject]);
 
     const onLoadImageHandler = () => {
-        setIsImageLoading(false);
+        // Flow: initial state (on component mount ): true -> when image is loaded onLoadHandler set state: false -> when day is clicked useEffect runs and set state -> true, when image is loaded onLoadHandler set state: false.
+        // Slow down setting of isImageLoading state to false because loading-skeleton have to be displayed for minimum 0.3 seconds(while state is true).
+        setTimeout(() => {
+            setIsImageLoading(false);
+        }, 500)
+
     }
 
     const time = currentHourObject.time.split(' ')[1];
     return (
         <div>
             <p>{time}</p>
-            {isImageLoading && <Skeleton className={globalStyles.imgSkeletonMedium} />}
-            <img className={isImageLoading ? globalStyles.displayNone : ''} src={currentHourObject.condition.icon} alt="Weather img" key={currentHourObject.time_epoch} onLoad={onLoadImageHandler} />
+            <div className={styles.animationContainer}>
+                <AnimatePresence>
+                    {
+                        isImageLoading && (
+                            <motion.div
+                                key="skeleton"
+                                className={styles.skeletonContainer}
+                                transition={{
+                                    duration: 0.3,
+                                    ease: easeInOut,
+                                }}
+
+                                initial={{
+                                    opacity: 1,
+
+                                }}
+
+                                exit={{
+                                    opacity: 0,
+                                }}
+                            >
+                                <Skeleton className={globalStyles.imgSkeletonMedium} />
+                            </motion.div>
+
+                        )
+                    }
+                </AnimatePresence>
+                <motion.div
+                    key="image"
+                    style={{
+                        // display none to prevent showing image before loading skeleton appears
+                        display: isImageLoading ? 'none' : 'block'
+                    }}
+                    transition={{
+                        duration: 0.3,
+                        delay: 0.2,
+                        ease: easeInOut,
+                    }}
+
+                    initial={{
+                        opacity: 0,
+                    }}
+
+                    animate={{
+                        // Ternary operator because image will mount in the same time with skeleton and animation will run under the skeleton.Target is to start animation when skeleton disappear.
+                        // When isImageLoading is false skeleton disappears and animation starts.
+                        opacity: isImageLoading ? 0 : 1,
+
+                    }}
+                >
+                    <img className={isImageLoading ? '' : ''} src={currentHourObject.condition.icon} alt="Weather img" key={currentHourObject.time_epoch} onLoad={onLoadImageHandler} />
+                </motion.div>
+            </div>
             <p>{unit === "C" ? `${currentHourObject.temp_c}° C` : `${currentHourObject.temp_f} °F`}</p>
         </div>
     )
