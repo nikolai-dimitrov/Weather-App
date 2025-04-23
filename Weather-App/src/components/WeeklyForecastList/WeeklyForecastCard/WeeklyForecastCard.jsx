6f69180153c78dce27e6d844ac97b712cf6b268c
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-04-23 19:58:30 +0300

    refactor: change weekly forecast loading skeleton logic
    WeeklyForecastCard takes care of rendering skeleton or content  instead of replacing WeeklyForecastCard with ForecastCardSkeleton as layout.

--- Weather-App/src/components/WeeklyForecastList/WeeklyForecastCard/WeeklyForecastCard.jsx
+++ Weather-App/src/components/WeeklyForecastList/WeeklyForecastCard/WeeklyForecastCard.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { AnimatePresence, motion, easeInOut } from "motion/react";
 import Skeleton from "react-loading-skeleton";
 
@@ -8,10 +8,35 @@ import 'react-loading-skeleton/dist/skeleton.css';
 import styles from "./weekly-forecast-card.module.css";
 import globalStyles from '../../../styles/global.module.css'
 
-export const WeeklyForecastCard = ({ unit, dailyWeatherData, changeHourlyForecastHandler, index }) => {
+export const WeeklyForecastCard = ({ unit, dailyWeatherData, changeHourlyForecastHandler, index, isLoading }) => {
     const [isImageLoading, setIsImageLoading] = useState(true);
+    const [showImageSkeleton, setShowImageSkeleton] = useState(true);
+    const imageSkeletonTimeoutRef = useRef(null);
+
+    useEffect(() => {
+        setIsImageLoading(true);
+        imageSkeletonTimeoutRef.current = setTimeout(() => {
+            if (isLoading || isImageLoading) {
+                // if data isn't loaded for less than 100ms - show loading skeleton
+                setShowImageSkeleton(true);
+            }
+        }, 100)
+
+        return () => clearTimeout(imageSkeletonTimeoutRef.current);
+
+    }, [isLoading]);
+
+    useEffect(() => {
+        // remove skeleton after data and image were loaded
+        if (!isLoading && !isImageLoading) {
+            setShowImageSkeleton(false);
+        };
+        
+    }, [isLoading, isImageLoading]);
 
     const onLoadImageHandler = (e) => {
+        // clearingTimeout prevents skeleton blinking when data loads fast.
+        clearTimeout(imageSkeletonTimeoutRef.current)
         setIsImageLoading(false);
     }
 
@@ -19,11 +44,14 @@ export const WeeklyForecastCard = ({ unit, dailyWeatherData, changeHourlyForecas
     return (
         <li>
             <div className={styles.cursorPointer} onClick={() => changeHourlyForecastHandler(index)}>
-                <p>{shortDayName}</p>
+                {isLoading ?
+                    <Skeleton height={20}></Skeleton> :
+                    <p>{shortDayName}</p>
+                }
                 <div className={styles.animationContainer}>
                     <AnimatePresence>
                         {
-                            isImageLoading && (
+                            showImageSkeleton && (
                                 <motion.div
                                     key="skeleton"
                                     className={styles.skeletonContainer}
@@ -51,7 +79,7 @@ export const WeeklyForecastCard = ({ unit, dailyWeatherData, changeHourlyForecas
                         key="image"
                         transition={{
                             duration: 0.3,
-                            delay: 0.2,
+                            delay: 0.3,
                             ease: easeInOut,
                         }}
 
@@ -60,17 +88,20 @@ export const WeeklyForecastCard = ({ unit, dailyWeatherData, changeHourlyForecas
                         }}
 
                         animate={{
-                            // Ternary operator because image will mount in the same time with skeleton and animation will run under the skeleton.Target is to start animation when skeleton disappear.
-                            // When isImageLoading is false skeleton disappears and animation starts.
+                            // trigger animation when image is already loaded 
                             opacity: isImageLoading ? 0 : 1,
-
                         }}
                     >
-                        <img src={dailyWeatherData.day.condition.icon} alt="Weather img" onLoad={onLoadImageHandler} />
+                        {/* Change image visibility instantly when isImageLoading state is set to true. 
+                        That prevents last image to be shown for a moment before skeleton appears. */}
+                        <img className={isImageLoading ? globalStyles.visibilityHidden : ''} src={dailyWeatherData.day.condition.icon} key={dailyWeatherData.day.avgtemp_f} alt="Weather img" onLoad={onLoadImageHandler} />
 
                     </motion.div>
                 </div>
-                <p>{unit === "C" ? `${dailyWeatherData.day.avgtemp_c}° C` : `${dailyWeatherData.day.avgtemp_f} °F`}</p>
+                {isLoading ?
+                    <Skeleton height={20}></Skeleton> :
+                    <p>{unit === "C" ? `${dailyWeatherData.day.avgtemp_c}° C` : `${dailyWeatherData.day.avgtemp_f} °F`}</p>
+                }
             </div>
         </li>
     )
--- Weather-App/src/components/WeeklyForecastList/WeeklyForecastList.jsx
+++ Weather-App/src/components/WeeklyForecastList/WeeklyForecastList.jsx
@@ -1,15 +1,31 @@
-import { memo} from "react";
+import { memo } from "react";
 import { WeeklyForecastCard } from "./WeeklyForecastCard/WeeklyForecastCard";
 
-export const WeeklyForecastList = memo(({ forecastday, unit, changeHourlyForecastHandler }) => {
+export const WeeklyForecastList = memo(({ forecastday, unit, changeHourlyForecastHandler, isLoading }) => {
     return (
         <ul>
             {forecastday?.map((dailyWeatherData, index) => {
                 return (
-                    <WeeklyForecastCard key={dailyWeatherData.date} unit={unit} dailyWeatherData={dailyWeatherData} changeHourlyForecastHandler={changeHourlyForecastHandler} index={index} />
+                    <WeeklyForecastCard key={dailyWeatherData.date} unit={unit} dailyWeatherData={dailyWeatherData} changeHourlyForecastHandler={changeHourlyForecastHandler} index={index} isLoading={isLoading} />
                 )
             })
             }
         </ul >
     )
 })
+
+
+
+    // useEffect(() => {
+    //     console.log("different image cdn runs index:", index)
+    //     console.log("state:",isImageLoading, index)
+
+    //     // setIsImageLoading(true);
+    //     timeoutRef.current = setTimeout(() => {
+    //         setIsImageLoading(true)
+    //         console.log('completed timeout', index)
+    //     }, 300)
+
+    //     return () => clearTimeout(timeoutRef.current)
+
+    // }, [dailyWeatherData.day.condition.icon]);
