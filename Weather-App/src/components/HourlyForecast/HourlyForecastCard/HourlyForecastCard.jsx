133d80472cffc955a5e4d3e477163b55b95995d7
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-04-25 19:04:20 +0300

    refactor: Implement minimum delay before showing image loading skeleton
    Instead of displaying image loading skeleton by default for 300ms on every re-render, display loading skeleton only when image loading requires more than 200ms.

--- Weather-App/src/components/HourlyForecast/HourlyForecastCard/HourlyForecastCard.jsx
+++ Weather-App/src/components/HourlyForecast/HourlyForecastCard/HourlyForecastCard.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { AnimatePresence, easeInOut, motion } from "motion/react";
 
 import Skeleton from "react-loading-skeleton";
@@ -6,80 +6,93 @@ import Skeleton from "react-loading-skeleton";
 import 'react-loading-skeleton/dist/skeleton.css';
 import styles from './hourly-forecast-card.module.css';
 import globalStyles from '../../../styles/global.module.css';
-export const HourlyForecastCard = ({ unit, currentHourObject }) => {
+export const HourlyForecastCard = ({ unit, currentHourObject, isLoading }) => {
     const [isImageLoading, setIsImageLoading] = useState(true);
+    const imageSkeletonDelayRef = useRef(null);
 
     useEffect(() => {
         setIsImageLoading(true);
+    }, [isLoading]);
+
+    useEffect(() => {
+        imageSkeletonDelayRef.current = setTimeout(() => {
+            setIsImageLoading(true);
+        }, 200);
+
+        return () => {
+            clearTimeout(imageSkeletonDelayRef.current);
+        }
+
     }, [currentHourObject]);
 
-    const onLoadImageHandler = () => {
-        // Flow: initial state (on component mount ): true -> when image is loaded onLoadHandler set state: false -> when day is clicked useEffect runs and set state -> true, when image is loaded onLoadHandler set state: false.
-        // Slow down setting of isImageLoading state to false because loading-skeleton have to be displayed for minimum 0.3 seconds(while state is true).
-        setTimeout(() => {
-            setIsImageLoading(false);
-        }, 500)
-
+    const onLoadImageHandler = (e) => {
+        clearTimeout(imageSkeletonDelayRef.current);
+        setIsImageLoading(false);
     }
+
 
     const time = currentHourObject.time.split(' ')[1];
     return (
-        <div>
-            <p>{time}</p>
-            <div className={styles.animationContainer}>
-                <AnimatePresence>
-                    {
-                        isImageLoading && (
-                            <motion.div
-                                key="skeleton"
-                                className={styles.skeletonContainer}
-                                transition={{
-                                    duration: 0.3,
-                                    ease: easeInOut,
-                                }}
+        <li>
+            <div>
+                {isLoading ?
+                    <Skeleton height={20} width={64}></Skeleton> :
+                    <p>{time}</p>
+                }
+                <div className={styles.animationContainer}>
+                    <AnimatePresence>
+                        {
+                            (isImageLoading || isLoading) && (
+                                <motion.div
+                                    key="skeleton"
+                                    className={styles.skeletonContainer}
+                                    transition={{
+                                        duration: 0.3,
+                                        ease: easeInOut,
+                                    }}
 
-                                initial={{
-                                    opacity: 1,
+                                    initial={{
+                                        opacity: 1,
 
-                                }}
+                                    }}
 
-                                exit={{
-                                    opacity: 0,
-                                }}
-                            >
-                                <Skeleton className={globalStyles.imgSkeletonMedium} />
-                            </motion.div>
+                                    exit={{
+                                        opacity: 0,
+                                    }}
+                                >
+                                    <Skeleton className={globalStyles.imgSkeletonMedium} />
+                                </motion.div>
 
-                        )
-                    }
-                </AnimatePresence>
-                <motion.div
-                    key="image"
-                    style={{
-                        // display none to prevent showing image before loading skeleton appears
-                        display: isImageLoading ? 'none' : 'block'
-                    }}
-                    transition={{
-                        duration: 0.3,
-                        delay: 0.2,
-                        ease: easeInOut,
-                    }}
+                            )
+                        }
+                    </AnimatePresence>
+                    <motion.div
+                        key="image"
+                        transition={{
+                            duration: 0.3,
+                            delay: 0.2,
+                            ease: easeInOut,
+                        }}
 
-                    initial={{
-                        opacity: 0,
-                    }}
+                        initial={{
+                            opacity: 0,
+                        }}
 
-                    animate={{
-                        // Ternary operator because image will mount in the same time with skeleton and animation will run under the skeleton.Target is to start animation when skeleton disappear.
-                        // When isImageLoading is false skeleton disappears and animation starts.
-                        opacity: isImageLoading ? 0 : 1,
+                        animate={{
+                            // When isImageLoading is false skeleton disappears and animation starts.
+                            opacity: isImageLoading ? 0 : 1,
 
-                    }}
-                >
-                    <img className={isImageLoading ? '' : ''} src={currentHourObject.condition.icon} alt="Weather img" key={currentHourObject.time_epoch} onLoad={onLoadImageHandler} />
-                </motion.div>
+                        }}
+                    >
+                        <img className={isLoading ? globalStyles.visibilityHidden : ''} src={currentHourObject.condition.icon} key={Date.now()} alt="Weather img" onLoad={onLoadImageHandler} />
+                    </motion.div>
+                </div>
+                {isLoading ?
+                    <Skeleton height={20} ></Skeleton> :
+                    <p>{unit === "C" ? `${currentHourObject.temp_c}° C` : `${currentHourObject.temp_f} °F`}</p>
+                }
+
             </div>
-            <p>{unit === "C" ? `${currentHourObject.temp_c}° C` : `${currentHourObject.temp_f} °F`}</p>
-        </div>
+        </li>
     )
 }
