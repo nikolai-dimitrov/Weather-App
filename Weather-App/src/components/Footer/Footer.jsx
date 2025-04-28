7f92a24e3296ed04ecd8d0acd31b3af21a7ea79a
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-04-28 17:27:55 +0300

    feat: implement footer

--- Weather-App/src/components/Footer/Footer.jsx
+++ Weather-App/src/components/Footer/Footer.jsx
@@ -1,6 +1,11 @@
+import { FaGithub } from "react-icons/fa";
+
+import styles from './footer.module.css';
 export const Footer = () => {
     return (
-        <>
-        </>
+        <div className={styles.footerContainer}>
+            <p>©All Rights Reserved. Weather Application 2025</p>
+            <a href="https://github.com/nikolai-dimitrov/"><FaGithub /></a>
+        </div>
     )
 }
