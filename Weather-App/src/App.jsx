8907e526a0067b61a64b9f8670a404c60f6a0add
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-05-02 13:27:03 +0300

    refactor: remove functionality from app and add context

--- Weather-App/src/App.jsx
+++ Weather-App/src/App.jsx
@@ -1,23 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { WeatherContext } from './contexts/WeatherContext';
 import { Navbar } from './components/Navbar/Navbar';
 import { Home } from './components/Home/Home';
 import { Footer } from './components/Footer/Footer';
 import { OpeningAnimation } from './components/OpeningAnimation/OpeningAnimation';
 
-import { extractWeatherData } from './services/weatherApiService';
 import { validateWeatherForm } from './validators/validateWeatherForm';
-import './App.css'
+import './App.css';
 
 function App() {
-  const [weatherData, setWeatherData] = useState(null);
-  const [queryString, setQueryString] = useState(null);
-  const [unit, setUnit] = useState("C");
+  const { weatherData, isLoading, updateQueryByGeolocation } = useContext(WeatherContext);
+  // const [queryString, setQueryString] = useState(null);
+  // const [unit, setUnit] = useState("C");
   const [showOpeningAnimation, setShowOpeningAnimation] = useState(true);
-  const [isLoading, setIsLoading] = useState(true);
-  const [showLoadingSkeleton, setShowLoadingSkeleton] = useState(false);
-  const [disableLocationBtn, setDisableLocationBtn] = useState(false);
-  const [error, setError] = useState(null);
-
+  // const [showLoadingSkeleton, setShowLoadingSkeleton] = useState(false);
+  // const [disableLocationBtn, setDisableLocationBtn] = useState(false);
+  // const [error, setError] = useState(null);
   useEffect(() => {
     const timeOut = setTimeout(() => setShowOpeningAnimation(false), 4000);
     // Get geolocation and update queryString with your current location on component mount.
@@ -25,90 +23,18 @@ function App() {
     return () => clearTimeout(timeOut);
   }, []);
 
-  useEffect(() => {
-    // Using showLoadingSkeleton && isLoading prevent skeleton blinking.Skeleton will be shown if fetch isn't completed for less than 300ms.
-    const skeletonDelay = setTimeout(() => setShowLoadingSkeleton(true), 300)
-
-    const fetchAndUpdateWeather = async () => {
-      if (!queryString) {
-        return;
-      }
-
-      setIsLoading(isLoading => true);
-      try {
-        const data = await extractWeatherData(queryString);
-        setWeatherData(data);
-        clearTimeout(skeletonDelay)
-        setIsLoading(isLoading => false);
-        setShowLoadingSkeleton(false);
-        setError(null);
-      } catch (error) {
-        setError(error.message);
-      };
-    };
-
-    fetchAndUpdateWeather();
-    return () => clearTimeout(skeletonDelay)
-  }, [queryString])
-
-  const updateQueryByGeolocation = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position["coords"];
-          setQueryString((queryString) => (`${latitude}, ${longitude}`));
-        },
-        (error) => {
-          setQueryString((queryString) => ('London'));
-          setDisableLocationBtn(true);
-          setError("Can not access your location!");
-        }
-      )
-    };
-  };
-
-  const searchFormSubmitHandler = (e, searchedLocation, clearInput) => {
-    e.preventDefault();
-    const isValid = validateWeatherForm(searchedLocation.location);
-    if (!isValid) {
-      return;
-    }
-    setQueryString((queryString) => (searchedLocation.location));
-    clearInput();
-  };
-
-  const geoLocationBtnClickHandler = () => {
-    updateQueryByGeolocation();
-  }
-
-  const clearError = () => {
-    setError(null);
-  };
-
-  const changeUnits = (newUnit) => {
-    if (newUnit != unit) {
-      setUnit((state) => (newUnit));
-    };
-  };
-
   return (
     <>
-
       {
         showOpeningAnimation ? (
           <OpeningAnimation />
         ) : (
           <>
             <header>
-              <Navbar
-                geoLocationBtnClickHandler={geoLocationBtnClickHandler}
-                searchFormSubmitHandler={searchFormSubmitHandler}
-                disableLocationBtn={disableLocationBtn}
-                changeUnits={changeUnits}
-              />
+              <Navbar />
             </header>
             <main>
-              <Home error={error} clearError={clearError} unit={unit} {...weatherData} isLoading={isLoading && showLoadingSkeleton} />
+              <Home />
             </main>
             <footer>
               <Footer />
